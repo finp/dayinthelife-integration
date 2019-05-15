8685ccdcd3a8c80f22809f936610eaa992970aca
Paul Wright <pwright@redhat.com>
2019-05-15 22:07:49 +0100

    netlify

---
+++ netlify-build.sh
@@ -0,0 +1,17 @@
+export DOC_DIR=docs
+export INDEX_DOC=index.adoc
+export NAV_DOC=docs/nav.adoc
+
+mkdir target
+mkdir target/modules
+mkdir target/modules/ROOT
+cp -Lr $DOC_DIR target/modules/ROOT/pages
+
+cp antora.yml target
+
+cp $NAV_DOC target/modules/ROOT/
+
+cp -Lr $INDEX_DOC target/modules/ROOT/pages
+
+
+node_modules/.bin/antora --html-url-extension-style=indexify --pull ../local-site.yml
